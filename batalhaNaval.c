9bcad64e30ffa36396398d27dde9add2fd0cee60
Kleber Inácio <202502588739@alunos.estacio.br>
2025-03-19 17:06:32 +0000

    Batalha Naval Nível Mestre Completo

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,62 +1,64 @@
 #include <stdio.h>
+ 
+ void aplicarHabilidade(int batnav[10][10], int origemX, int origemY, int poder) {
+     int matrizHab[5][5] = {0};
+     
+     // Código das habilidades
+     if (poder == 1) { // Cone
+         matrizHab[0][2] = 1;
+         matrizHab[1][1] = matrizHab[1][2] = matrizHab[1][3] = 1;
+         for (int i = 0; i < 5; i++) matrizHab[2][i] = 1;
+     } else if (poder == 2) { // Cruz
+         for (int i = 0; i < 5; i++) matrizHab[2][i] = matrizHab[i][2] = 1;
+     } else if (poder == 3) { // Octaedro
+         matrizHab[0][2] = matrizHab[4][2] = 1;
+         matrizHab[1][1] = matrizHab[1][3] = matrizHab[3][1] = matrizHab[3][3] = 1;
+         matrizHab[1][2] = matrizHab[3][2] = 1;
+         for (int i = 0; i < 5; i++) matrizHab[2][i] = 1;
+     }
+     
+     // Aplicação das habilidades
+     for (int i = 0; i < 5; i++) {
+         for (int j = 0; j < 5; j++) {
+             int x = origemX + i - 2;
+             int y = origemY + j - 2;
+             if (x >= 0 && x < 10 && y >= 0 && y < 10 && matrizHab[i][j] == 1) {
+                batnav[x][y] = 5;
+             }
+         }
+     }
+ }
+ 
+ int main() {
+    char colunas[12] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+     int batnav[10][10] = {0};
 
-int main(){
-    char colunas[12] = {' ', ' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-    int batnav [10][10];
-    char *linhas[10] = {" 1- ", " 2- ", " 3- ", " 4- ", " 5- ", " 6- ", " 7- ", " 8- ", " 9- ", "10- "};
-
-    //Implementação das letras das colunas;
-    for(int j = 0; j < 12; j++){
+     for(int j = 0; j < 12; j++){
         printf("%c ", colunas[j]);
     }
     printf("\n");
-
-    
-    //Implementação do campo batalha naval
-    for (int i = 0; i < 10; i++){
-        for(int j = 0; j < 10; j++){
-            //Implementação dos números das linhas;
-            if(i >=0 && i <= 10 && j == 0){
-                printf("%s", linhas[i]);
-            }   
-
-            //Implementação do Navio 1;
-            if(i == 3 && j >= 3 && j <= 5){
-                printf("3 ");
-                }
-                if(i == 3 && j >= 3 && j <= 5){
-                    continue;
-                    }
-
-                    //Implementação do Navio 2;
-                    if(i >= 7 && i <= 9 && j == 2){
-                        printf("3 ");
-                    }
-                        if(i >= 7 && i <= 9 && j == 2){
-                            continue;
-                            }
-                        //Implementação do Navio 3
-                    if(i == j && i >= 5 && i <= 7){
-                        printf("3 ");
-                    }
-                        if(i == j && i >= 5 && i <= 7){
-                            continue;
-                            }
-                               //Implementação do Navio 4
-                    if(i + j == 9 && i >= 0 && i <= 2){
-                        printf("3 ");
-                    }
-                        if(i + j == 9 && i >= 0 && i <= 2){
-                            continue;
-                            }
-                                                
-
-                printf("0 ");
-        }
-        printf("\n");
-    }
-
-    return 0;
-
-}
-
+     
+     // Posicionamento dos navios
+     int posNavios[4][2] = {{0, 5}, {6, 2}, {4, 4}, {7, 5}};
+     for (int i = 0; i < 3; i++) {
+         batnav[posNavios[0][0]][posNavios[0][1] + i] = 3; // Navio Horizontal
+         batnav[posNavios[1][0] + i][posNavios[1][1]] = 3; // Navio Vertical
+         batnav[posNavios[2][0] + i][posNavios[2][1] + i] = 3; // Navio Diagonal 1
+         batnav[posNavios[3][0] + i][posNavios[3][1] - i] = 3; // Navio Diagonal 2
+     }
+     
+     // Aplicação das habilidades
+     aplicarHabilidade(batnav, 3, 3, 1); // Cone
+     aplicarHabilidade(batnav, 6, 6, 2); // Cruz
+     aplicarHabilidade(batnav, 2, 7, 3); // Octaedro
+     
+     // Exibir tabuleiro
+     for (int i = 0; i < 10; i++) {
+         for (int j = 0; j < 10; j++) {
+             printf("%d ", batnav[i][j]);
+         }
+         printf("\n");
+     }
+ 
+     return 0;
+ }
